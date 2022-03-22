cd8a768b6999e83a3c92b242d2b4be3c5837d38a
Dips2001 <101541717+Dips2001@users.noreply.github.com>
2022-03-23 01:43:05 +0530

    Update OpenCartRunner.java

--- src/test/java/com/runner/OpenCartRunner.java
+++ src/test/java/com/runner/OpenCartRunner.java
@@ -27,7 +27,7 @@ import io.cucumber.testng.PickleWrapper;
 
 @CucumberOptions(
 features="Features",
-tags="@StoreLogo",
+tags="@Home",
 glue = "com.Home.stepDefinition",
 plugin={"html:target/cucumber-html-report", "json:target/cucumber.json","pretty:target/cucumber-pretty.txt","usage:target/cucumber-usage.json", "junit:target/cucumber-results.xml"},
 dryRun = false,
